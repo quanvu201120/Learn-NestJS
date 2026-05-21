782a37610f97dca0140945b1bfb8d2b36f4d5677
quanvu201120 <vuquan201120@gmail.com>
2026-05-21 17:13:48 +0700

    update delete user

---
+++ src/common/transform.interceptor.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable prettier/prettier */
+import {
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+    statusCode: number;
+    message?: string;
+    data: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<
+    T,
+    Response<T>
+> {
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler,
+    ): Observable<Response<T>> {
+        const response = context.switchToHttp().getResponse();
+        const statusCode = response.statusCode;
+
+        return next.handle().pipe(
+            map((data) => ({
+                statusCode: statusCode,
+                message: data?.message || 'Thực hiện tác vụ thành công!',
+                data: data?.result !== undefined ? data.result : data,
+                meta: data?.meta !== undefined ? data.meta : undefined,
+            })),
+        );
+    }
+}
